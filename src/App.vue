48e478cb74d03801be945722ce95263bf11bb49e
Guillber <guillbermndz@gmail.com>
2022-02-25 22:22:52 -0600

    axios client method get

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
- <h1 class="title">HelloWorld</h1>
+  <div class="bg-zinc-800 h-16">
+    <h1>Rick and Morty App</h1>
+  </div>
+    <div v-for="character in characters" :key="character.id">
+      {{ character.name }}
+    </div>
+  
 </template>
 
 <script>
+//librerias
+import axios from "axios";
 
 export default {
-  name: 'App',
-  async mounted() {
-   const res = await fetch('https://rickandmortyapi.com/api/character')
-   const data = await res.json()
-   console.log(data)
+  name: "App",
+  data() {
+    return {
+      //se crea una variable que es un arreglo vacio, para al macenar la info
+      characters: [],
+    };
+  },
+  mounted() {
+    axios
+      //se hace una peticion a la API
+      .get("https://rickandmortyapi.com/api/character")
+      //se captura la respuesta
+      .then((res) => {
+        //se guarda la informacion en el arreglo vacio
+        this.characters = res.data.results;
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
