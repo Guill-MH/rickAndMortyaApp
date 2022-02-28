398ff84e5c639b062accb3ac1f194e20337b3821
Guillber <guillbermndz@gmail.com>
2022-02-28 00:27:06 -0600

    actualizacion de cards

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="bg-zinc-800 h-16">
-    <h1>Rick and Morty App</h1>
+  <div class="bg-slate-700 h-16 flex items-center shadow-xl">
+    <img
+      v-bind:src="require('@/assets/log.png')"
+      alt=""
+      class="w-16 md:w-31 lg:w-31"
+    />
+    <h1 class="font-bold text-2xl text-slate-300">Rick and Morty App</h1>
   </div>
-    <div v-for="character in characters" :key="character.id">
-      {{ character.name }}
-    </div>
-  
+  <div class="w-full slate-300  flex flex-wrap justify-center p-3 bg-slate-900 ">
+    <MyCharacter
+      v-for="character in characters"
+      v-bind:key="character.id"
+      v-bind:character="character"
+    />
+  </div>
 </template>
 
 <script>
 //librerias
 import axios from "axios";
+//Componentes
+import MyCharacter from "./components/MyCharacters.vue";
 
 export default {
   name: "App",
+  components: {
+    MyCharacter, //character
+  },
   data() {
     return {
       //se crea una variable que es un arreglo vacio, para al macenar la info
---
+++ src/components/MyCharacters.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-80  m-auto p-4 mb-10 flex justify-center rounded-md font-mono bg-slate-700 shadow-lg shadow-lime-500/40">
+    <div class="flex flex-col ">
+      <img v-bind:src="character.image" alt="personaje" class="rounded-md" />
+      <p>Name: {{ character.name }}</p>
+      <p>Sex: {{ character.gender }}</p>
+      <p>Species: {{ character.species }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["character"],
+};
+</script>
